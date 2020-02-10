683d062840f9dba8232918dc134b804d36855912
kaminaritukane <kaminaritukane@163.com>
2020-02-10 14:27:43 +0800

    Try add physX callback when collision happened

--- PhysicsDemo/Simulation/simulate.cc
+++ PhysicsDemo/Simulation/simulate.cc
@@ -5,6 +5,10 @@
 #include <aether/common/net.hh>
 #include <aether/common/base_protocol.hh>
 #include <aether/common/random.hh>
+#include <unordered_map>
+#include<typeinfo>
+#include<cstring>
+
 
 using physx::PxVec3, physx::PxPlane, physx::PxU32, physx::PxTransform,
       physx::PxReal, physx::PxCreateStatic, physx::PxQuat;
@@ -38,6 +42,54 @@ struct physx_update_system {
         physx_state->scene->fetchResults(true);
     }
 };
+
+std::unordered_map<physx::PxActor*, user_cell_state::agent_reference> gPxActorToEntity;
+
+class ContactReportCallback: public ::physx::PxSimulationEventCallback
+{
+	void onConstraintBreak(physx::PxConstraintInfo* constraints, physx::PxU32 count)	{ PX_UNUSED(constraints); PX_UNUSED(count); }
+	void onWake(physx::PxActor** actors, physx::PxU32 count)							{ PX_UNUSED(actors); PX_UNUSED(count); }
+	void onSleep(physx::PxActor** actors, physx::PxU32 count)							{ PX_UNUSED(actors); PX_UNUSED(count); }
+	void onTrigger(physx::PxTriggerPair* pairs, physx::PxU32 count)					{ PX_UNUSED(pairs); PX_UNUSED(count); }
+	void onAdvance(const physx::PxRigidBody*const*, const physx::PxTransform*, const physx::PxU32) {}
+	void onContact(const physx::PxContactPairHeader& pairHeader, const physx::PxContactPair* pairs, physx::PxU32 nbPairs) 
+	{
+		for(physx::PxU32 i=0; i < nbPairs; i++)
+        {
+            const physx::PxContactPair& cp = pairs[i];
+
+            if(cp.events & physx::PxPairFlag::eNOTIFY_TOUCH_FOUND)
+            {
+                physx::PxActor* actor0 = pairHeader.actors[0];
+                physx::PxActor* actor1 = pairHeader.actors[1];
+                printf("actor0: %d, actor1: %d\n", actor0, actor1);
+
+                auto it0 = gPxActorToEntity.find(actor0);
+                printf("it0 found: %d\n", it0 != gPxActorToEntity.end());
+                if ( it0 != gPxActorToEntity.end() )
+                {
+                    auto agent0 = it0->second;
+                    auto triComp = agent0.get_dynamic<c_trivial>();
+                    triComp->agent_colour = {0.0f, 0.0f, 1.0f};
+                    //agent0.set
+                    printf("Set agent0: %d color\n", triComp->id);
+                }
+
+                auto it1 = gPxActorToEntity.find(actor1);
+                printf("it1 found: %d\n", it1 != gPxActorToEntity.end());
+                if ( it1 != gPxActorToEntity.end() )
+                {
+                    auto agent1 = it1->second;
+                    auto triComp = agent1.get_dynamic<c_trivial>();
+                    triComp->agent_colour =  {0.0f, 0.0f, 1.0f};
+                    printf("Set agent1: %d color\n", triComp->id);
+                }
+            }
+        }
+	}
+};
+
+ContactReportCallback gContactReportCallback;
 
 // This is the cell tick function as described in main.cc. It calls the ECS internal tick function
 void cell_tick(const aether_cell_state<octree_traits> &aether_state, user_cell_state &state, float delta_time) {
@@ -97,17 +149,22 @@ void initialise_world(const aether_cell_state<octree_traits> &aether_state, user
     const auto cell = aether_state.get_cell();
     aether::physx::physx_state *physx_state = static_cast<aether::physx::physx_state*>(state.user_data);;
 
+    physx_state->scene->setSimulationEventCallback(&gContactReportCallback);
+
     // static friction, dynamic friction, restitution; COR = 1 means perfectly elastic collision
     // NOTE: currently serializer requires every entity to have a separate material and shape, so identical materials
     // and shapes must be defined for each entity
-    for(int i = 0; i < 500; i++){
-      float size_rnd = generate_random_f32();
-      float size = (size_rnd*3) + 1;
+    for(int i = 0; i < 10; i++){
+      //float size_rnd = generate_random_f32();
+      float size = 1.0f;//(size_rnd*3) + 1;
       PxReal extent(size);
       physx::PxMaterial* material = physx_state->physics->createMaterial(0.0f, 0.0f, 1.0f);
       physx::PxShape* actor_shape = physx_state->physics->createShape(physx::PxBoxGeometry(extent, extent, extent), *material);
 
-      physx::PxTransform actor_position = physx::PxTransform(physx::PxVec3((generate_random_f32()-0.5f)*200, (generate_random_f32()-0.5f)*200, (generate_random_f32()-0.5f)*200));
+      physx::PxTransform actor_position = physx::PxTransform(physx::PxVec3((generate_random_f32()-0.5f)*200, 
+        (generate_random_f32()-0.5f)*200, 
+        (generate_random_f32()-0.5f)*200)
+      );
       physx::PxRigidDynamic* actor =  physx_state->physics->createRigidDynamic(actor_position);
       actor->attachShape(*actor_shape);
       physx::PxRigidBodyExt::updateMassAndInertia(*actor, 10.0f);
@@ -122,7 +179,12 @@ void initialise_world(const aether_cell_state<octree_traits> &aether_state, user
       auto trivial = agent.create_component<c_trivial>();
       trivial->id = i;
       trivial->size = size;
-      trivial->agent_colour.r *= size_rnd;
+      //trivial->agent_colour.r *= size_rnd;
+
+      //const std::type_info& typeOfAgent = typeid(agent);
+      //printf("agent type: %s\n", typeOfAgent.name());
+
+      gPxActorToEntity.insert({ dynamic_cast<physx::PxActor*>(actor), agent});
 
       // Unlike most physX applications, we need to maintain a shape and material for each entity,
       // rather than using the same material across many entities.
@@ -177,4 +239,5 @@ void cell_state_serialize(const aether_cell_state<octree_traits>& aether_state, 
 
 // We have no input in this simulation, so this remains empty
 void handle_events(const aether_cell_state<octree_traits> &aether_state, user_cell_state &state, message_reader_type &reader) {
+    //printf("11111111.\n");
 }
